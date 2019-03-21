a3f89fcb4a2de3bfb7bc7f0d9e1f99c023862fea
harry-z <shinji_akari@163.com>
2019-03-21 14:19:24 +0800

    基本CMake编译环境

---
+++ BuildVS2015.bat
@@ -0,0 +1,9 @@
+@echo off
+mkdir buildVS2015
+pushd buildVS2015
+@echo on
+cmake ../ -G "Visual Studio 14 2015 Win64" -Thost=x64
+cmake --build . --config %1
+@echo off
+popd
+pause
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::cout << "Hello 3DApp" << std::endl;
+    system("pause");
+    return 0;
+}

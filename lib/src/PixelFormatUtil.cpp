68b225f333b49b4847348d3ccb9e393a3c392901
harry-z <shinji_akari@163.com>
2019-04-19 17:16:47 +0800

    增加PixelFormatUtil

--- lib/src/PixelFormatUtil.cpp
+++ lib/src/PixelFormatUtil.cpp
@@ -2,39 +2,39 @@
 
 dword GetPixelFormatStride(EPixelFormat format) {
 	switch (format) {
-	case EPixelFormat_Unknown:
-	case EPixelFormat_BC1:
-	case EPixelFormat_BC2:
-	case EPixelFormat_BC3:
-	case EPixelFormat_BC4:
-	case EPixelFormat_BC5:
-	case EPixelFormat_BC6H:
-	case EPixelFormat_BC7:
-	case EPixelFormat_Depth16:
-	case EPixelFormat_Depth24:
-	case EPixelFormat_Depth32:
-	case EPixelFormat_R9G9B9E5_SharedExp:
+	case EPixelFormat::EPixelFormat_Unknown:
+	case EPixelFormat::EPixelFormat_BC1:
+	case EPixelFormat::EPixelFormat_BC2:
+	case EPixelFormat::EPixelFormat_BC3:
+	case EPixelFormat::EPixelFormat_BC4:
+	case EPixelFormat::EPixelFormat_BC5:
+	case EPixelFormat::EPixelFormat_BC6H:
+	case EPixelFormat::EPixelFormat_BC7:
+	case EPixelFormat::EPixelFormat_Depth16:
+	case EPixelFormat::EPixelFormat_Depth24:
+	case EPixelFormat::EPixelFormat_Depth32:
+	case EPixelFormat::EPixelFormat_R9G9B9E5_SharedExp:
 	default:
 		return 0;
-	case EPixelFormat_L8:
-	case EPixelFormat_A8:
+	case EPixelFormat::EPixelFormat_L8:
+	case EPixelFormat::EPixelFormat_A8:
 		return 1;
-	case EPixelFormat_A8L8:
-	case EPixelFormat_F16_R:
+	case EPixelFormat::EPixelFormat_A8L8:
+	case EPixelFormat::EPixelFormat_F16_R:
 		return 2;
-	case EPixelFormat_A2R10G10B10:
-	case EPixelFormat_X8R8G8B8:
-	case EPixelFormat_A8R8G8B8:
+	case EPixelFormat::EPixelFormat_A2R10G10B10:
+	case EPixelFormat::EPixelFormat_X8R8G8B8:
+	case EPixelFormat::EPixelFormat_A8R8G8B8:
 #if defined(RENDERAPI_DX9)
-	case EPixelFormat_R8G8B8:
+	case EPixelFormat::EPixelFormat_R8G8B8:
 #endif
-	case EPixelFormat_F16_RG:
-	case EPixelFormat_F32_R:
+	case EPixelFormat::EPixelFormat_F16_RG:
+	case EPixelFormat::EPixelFormat_F32_R:
 		return 4;
-	case EPixelFormat_F16_RGBA:
-	case EPixelFormat_F32_RG:
+	case EPixelFormat::EPixelFormat_F16_RGBA:
+	case EPixelFormat::EPixelFormat_F32_RG:
 		return 8;
-	case EPixelFormat_F32_RGBA:
+	case EPixelFormat::EPixelFormat_F32_RGBA:
 		return 16;
 	}
 }
@@ -46,15 +46,15 @@ void GetSurfaceInfo(word nWidth, word nHeight, byte nDepth, EPixelFormat eFormat
 	dword nbcNumBytesPerBlock = 0;
 	switch (eFormat)
 	{
-	case EPixelFormat_BC1:
-	case EPixelFormat_BC4:
+	case EPixelFormat::EPixelFormat_BC1:
+	case EPixelFormat::EPixelFormat_BC4:
 		nbcNumBytesPerBlock = 8;
 		break;
-	case EPixelFormat_BC2:
-	case EPixelFormat_BC3:
-	case EPixelFormat_BC5:
-	case EPixelFormat_BC6H:
-	case EPixelFormat_BC7:
+	case EPixelFormat::EPixelFormat_BC2:
+	case EPixelFormat::EPixelFormat_BC3:
+	case EPixelFormat::EPixelFormat_BC5:
+	case EPixelFormat::EPixelFormat_BC6H:
+	case EPixelFormat::EPixelFormat_BC7:
 		nbcNumBytesPerBlock = 16;
 		break;
 	default:
